73d1bd0987598d3d6e11e0a50f34bf9544fff4f6
danie <greenhalghdan@gmail.com>
2024-01-04 22:00:25 +0000

    finished

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-import requests
+from pdfquery import PDFQuery
+from boto3 import Session
 
-#text to speech
-#http://www.ispeech.org/api/#introduction
+#requires the aws cli and to configure it
 
-#read data from a PDF
-#https://www.freecodecamp.org/news/extract-data-from-pdf-files-with-python/
+pdf = PDFQuery('sample.pdf')
+pdf.load()
+text_elements = pdf.pq('LTTextLineHorizontal')
+text = [t.text for t in text_elements]
+
+print(text)
+text = ' '.join(text)
+print(text)
+
+session = Session(profile_name="default")
+polly = session.client("polly")
+
+response = polly.start_speech_synthesis_task(
+        LanguageCode='en-US',
+        OutputFormat='mp3',
+        OutputS3BucketName='billingdanielgreenhalgh.com',
+        Text=text,
+        TextType= 'text',
+        VoiceId='Amy'
+    )
+
+print(response)
+print(f"You can download your file here {response['SynthesisTask']['OutputUri']}")
+
